4a19a7c560feb9a5df86ca8df521613a83d5f27a
AlbiXD <albizhaku15@gmail.com>
2024-03-27 03:11:28 -0400

    Added server/client folders

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express()
+
+app.get("/api", (req,res) =>{
+    res.json({"users":["userOne", "userTwo", "userThree"]})
+})
+
+
+app.listen(5000, ()=> {console.log("Server Started on Port 5000")})
